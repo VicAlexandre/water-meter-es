5f42749a8af0012401e6fc5325efdd054aa1bfe5
Victor <varm@ic.ufal.br>
2024-11-28 00:48:03 -0300

    feat: adiciona inicialização da thread de data manager

--- src/main.c
+++ src/main.c
@@ -7,10 +7,14 @@
 
 int main(void)
 {
+	struct data_man_conf conf = {
+		.poll_interval = K_MSEC(CONFIG_POLLING_DELAY_MS),
+		.alarm_threshold = 10,
+		.critical_alarm_threshold = 5,
+	};
 	LM_CHECK_ERROR(ble_init());
+	LM_CHECK_ERROR(data_man_init(10, &conf));
 	LM_CHECK_ERROR(hcsr04_init());
-
-	printf("Hello World!\n");
 
 	return 0;
 }
